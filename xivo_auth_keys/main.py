6043bfbc1adb00270cef2916bad9629035f434ba
François Blackburn <fblackburn@avencall.com>
2016-01-19 09:48:58 -0500

    sync key file with db
    The functionnality of xivo-auth-keys has been changed. It no longer
    generates keys. This script reads the database and writes the results in
    files

--- xivo_auth_keys/main.py
+++ xivo_auth_keys/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2015 Avencall
+# Copyright (C) 2015-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,45 +15,37 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import argparse
 import os
-import uuid
 import yaml
+import xivo_dao
+
+from xivo_dao.helpers.db_utils import session_scope
+from xivo_dao.accesswebservice_dao import get_services
 
 
-def main(argv):
-    service_id = _parse_cli_args(argv).service_id
+def main():
+    xivo_dao.init_db_from_config()
     key_updater = _KeyUpdater()
 
     os.umask(027)
-    key_updater.update(service_id)
+    for service in _get_services():
+        key_updater.update(service.login, service.passwd)
+
+
+def _get_services():
+    with session_scope():
+        return get_services()
 
 
 class _KeyUpdater(object):
 
-    def __init__(self):
-        self.filename = '/var/lib/xivo-auth-keys/{service_id}-key.yml'
+    filename = '/var/lib/xivo-auth-keys/{service_id}-key.yml'
 
-    def update(self, service_id):
+    def update(self, service_id, service_key):
         filename = self.filename.format(service_id=service_id)
-        if not self._is_file_exist(filename):
-            service_key = self._generate_key()
-            self._write_config_file(filename, service_id, service_key)
-
-    def _generate_key(self):
-        return str(uuid.uuid4())
-
-    def _is_file_exist(self, filename):
-        return os.path.isfile(filename)
+        self._write_config_file(filename, service_id, service_key)
 
     def _write_config_file(self, filename, service_id, service_key):
         with open(filename, 'w') as fobj:
             yaml.safe_dump({'service_id': service_id,
                             'service_key': service_key}, fobj)
-
-
-def _parse_cli_args(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('service_id',
-                        help="The id of the service for which the key will be generated")
-    return parser.parse_args(argv)
